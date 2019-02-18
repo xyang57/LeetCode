8fb16b375733a2fe56fddaeb766c8123c7304f09
XuYang <albertyangxu@gmail.com>
2019-02-18 02:26:29 -0500

    update leet 70

--- Leet70.py
+++ Leet70.py
@@ -27,7 +27,7 @@ Explanation: There are three ways to climb to the top.
 Solution1:
     This problem is the same as Fibonacci Number: Fib(n) = Fib(n-1) + Fib(n-2)
 """
-def climbStairs('int') -> 'int':
+def climbStairs(n: 'int') -> 'int':
     if n == 1:
         return 1
     first = 1
@@ -40,4 +40,41 @@ def climbStairs('int') -> 'int':
     return second
 # Time complexity: O(n); space complexity: O(1)
     
+"""
+Solution 2:
+    Recursion with memoization
+"""
 
+def climbStairs(n, memo = {}):
+    """
+    :type n: int
+    :rtype: int
+    """
+        
+    if n==1:
+        return 1
+    if n == 2:
+        return 2
+    try:
+        return memo[n]
+    except KeyError:
+        result = climbStairs(n-1, memo) + climbStairs(n-2, memo)
+        memo[n] = result
+        return result
+# Time complexity: O(n); space complexity: O(n)
+        
+"""
+Solution 3:
+    Dynamic programming
+"""
+def climbStairs(n: 'int') -> 'int':
+    if n == 1:
+        return 1
+    dp = [0]*n
+    dp[0] = 1
+    dp[1] = 2
+    if n >= 3:
+        for i in range(2,n):
+            dp[i] = dp[i-1] + dp[i-2]
+    return dp[n-1]
+# Time complexity: O(n); space complexity: O(n)
