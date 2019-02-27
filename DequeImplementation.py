474ef42af638e2303e0854605700f56da3c5d4f6
XuYang <albertyangxu@gmail.com>
2019-02-27 11:41:24 -0500

    add DequeImplementation

---
+++ DequeImplementation.py
@@ -0,0 +1,25 @@
+class Deque(object):
+    
+    def __init__(self):
+        self.items = []
+    
+    def addFront(self, item):
+        self.items.insert(0,item)
+    
+    def addRear(self, item):
+        self.items.append(item)
+        
+    def removeFront(self):
+        return self.items.pop(0)
+    
+    def removeRear(self):
+        return self.items.pop()
+    
+    def isEmpty(self):
+        return self.items == []
+    
+    def size(self):
+        return len(self.items)
+    
+    def __str__(self):
+        return str(self.items)
