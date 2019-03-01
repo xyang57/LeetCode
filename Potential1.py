35627c8450d19feac8afa8daf33520d8c8afded7
XuYang <albertyangxu@gmail.com>
2019-03-01 12:03:41 -0500

    update

--- Potential1.py
+++ Potential1.py
@@ -6,39 +6,31 @@ For example,
 
 Given [[0, 30],[5, 10],[15, 20]],
 return false.
+
 """
-def meetingRoom1(times):
-    times.sort()
-    for i in range(len(times)-2):
-        if times[i+1][0] < times[i][1]:
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+"""
+Solution1: brute force
+Solution2: sort first then make comparisons
+"""
+def canAttendMeetings(intervals):
+    """
+    input: intervals: list of Interval
+    output: Ture or False
+    """
+    intervals.sort(key = lambda k: k.start )
+    for i in range(len(intervals) - 1):
+        if intervals[i].end > intervals[i+1].start:
             return False
     return True
 
-times = [[0,30],[5,10],[15,20]]
-print(meetingRoom(times))
-
-"""
-Question 2
-
-Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
-
-For example,
-
-Given [[0, 30],[5, 10],[15, 20]],
-
-return 2.
-"""
-def meetingRoom2(times):
-    times.sort()
-    result = [[]]
-    if len(times) == 1:
-        return 1
-    if len(times) == 0:
-        return 0
-    result[0].append(times[1])
-    for i in range(1,len(times)):
-        l = len(result)
-        for j in range(l):
+# Time complexity: O(nlogn); space complexity: O(1)
             
             
 
